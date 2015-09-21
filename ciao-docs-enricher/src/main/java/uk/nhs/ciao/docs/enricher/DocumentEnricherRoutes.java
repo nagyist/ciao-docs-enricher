2d2e62b07bcdf3457368efe4d8506402489a5fbf
Iestyn Evans <Iestyn.Evans@bjss.com>
2015-09-21 11:50:26 +0100

    Added in progress directory manager route and CIAO property Moved enrich document route building into separate class (called from main route)

--- ciao-docs-enricher/src/main/java/uk/nhs/ciao/docs/enricher/DocumentEnricherRoutes.java
+++ ciao-docs-enricher/src/main/java/uk/nhs/ciao/docs/enricher/DocumentEnricherRoutes.java
@@ -1,17 +1,13 @@
 package uk.nhs.ciao.docs.enricher;
 
 
-import org.apache.camel.LoggingLevel;
-import org.apache.camel.model.dataformat.JsonLibrary;
-import org.apache.camel.spi.Registry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
 
-import uk.nhs.ciao.CIPRoutes;
 import uk.nhs.ciao.camel.CamelApplication;
 import uk.nhs.ciao.configuration.CIAOConfig;
-import uk.nhs.ciao.docs.parser.ParsedDocument;
-import uk.nhs.ciao.exceptions.CIAOConfigurationException;
+import uk.nhs.ciao.docs.enricher.route.EnrichDocumentRoute;
+import uk.nhs.ciao.docs.parser.route.InProgressFolderManagerRoute;
 
 /**
  * Configures multiple camel document parser routes determined by properties specified
@@ -35,105 +31,40 @@ import uk.nhs.ciao.exceptions.CIAOConfigurationException;
  * <dt>outputQueue<dt>
  * <dd>The name of the queue output messages should be sent to</dd>
  */
-public class DocumentEnricherRoutes extends CIPRoutes {
-	private static final Logger LOGGER = LoggerFactory.getLogger(DocumentEnricherRoutes.class);
-	
-	/**
-	 * The root property 
-	 */
-	public static final String ROOT_PROPERTY = "documentEnricherRoutes";
-	
+public class DocumentEnricherRoutes implements RoutesBuilder {	
 	/**
 	 * Creates multiple document parser routes
 	 * 
-	 * @throws RuntimeException If required CIAO-config properties are missing
+	 * @throws Exception If required CIAO-config properties are missing
 	 */
 	@Override
-	public void configure() {
-		super.configure();
+	public void addRoutesToCamelContext(final CamelContext context) throws Exception {
+		addEnrichDocumentRoutes(context);
 		
-		final CIAOConfig config = CamelApplication.getConfig(getContext());
+		// services
+		addInProgressFolderManagerRoute(context);
+	}
+	
+	private void addEnrichDocumentRoutes(final CamelContext context) throws Exception {
+		final CIAOConfig config = CamelApplication.getConfig(context);
 		
-		try {
-			final String[] routeNames = config.getConfigValue(ROOT_PROPERTY).split(",");
-			for (final String routeName: routeNames) {
-				final ParseDocumentRouteBuilder builder = new ParseDocumentRouteBuilder(
-						routeName, config);
-				builder.configure();
-			}
-		} catch (CIAOConfigurationException e) {
-			throw new RuntimeException("Unable to build routes from CIAOConfig", e);
+		final String[] routeNames = config.getConfigValue(EnrichDocumentRoute.ROOT_PROPERTY).split(",");
+		for (final String routeName: routeNames) {
+			final EnrichDocumentRoute route = new EnrichDocumentRoute(
+					routeName, config);
+			route.setInProgressFolderManagerUri("direct:in-progress-folder-manager");
+			
+			context.addRoutes(route);
 		}
 	}
 	
-	/**
-	 * Creates a Camel route for the specified name / property prefix.
-	 * <p>
-	 * Each configurable property is determined by:
-	 * <ul>
-	 * <li>Try the specific property: <code>${ROOT_PROPERTY}.${name}.${propertyName}</code></li>
-	 * <li>If missing fallback to: <code>${ROOT_PROPERTY}.${propertyName}</code></li>
-	 * </ul>
-	 */
-	private class ParseDocumentRouteBuilder {
-		private final String name;
-		private final String inputQueue;
-		private final String enricherId;
-		private final String outputQueue;
+	private void addInProgressFolderManagerRoute(final CamelContext context) throws Exception {
+		final InProgressFolderManagerRoute route = new InProgressFolderManagerRoute();
 		
-		/**
-		 * Creates a new route builder for the specified name / property prefix
-		 * 
-		 * @param name The route name / property prefix
-		 * @throws CIAOConfigurationException If required properties were missing
-		 */
-		public ParseDocumentRouteBuilder(final String name, final CIAOConfig config) throws CIAOConfigurationException {
-			this.name = name;
-			this.inputQueue = findProperty(config, "inputQueue");
-			this.enricherId = findProperty(config, "enricherId");
-			this.outputQueue = findProperty(config, "outputQueue");
-		}
+		route.setInProgressFolderManagerUri("direct:in-progress-folder-manager");
+		route.setInternalRoutePrefix("in-progress-folder-manager");
+		route.setInProgressFolderRootUri("file:{{inProgressFolder}}");
 		
-		/**
-		 * Try the specific 'named' property then fall back to the general 'all-routes' property
-		 */
-		private String findProperty(final CIAOConfig config, final String propertyName) throws CIAOConfigurationException {
-			final String specificName = ROOT_PROPERTY + "." + name + "." + propertyName;
-			final String genericName = ROOT_PROPERTY + "." + propertyName;
-			if (config.getConfigKeys().contains(specificName)) {
-				return config.getConfigValue(specificName);
-			} else if (config.getConfigKeys().contains(genericName)) {
-				
-				return config.getConfigValue(genericName);
-			} else {
-				throw new CIAOConfigurationException("Could not find property " + propertyName +
-						" for route " + name);
-			}
-		}
-
-		/**
-		 * Configures / creates a new Camel route corresponding to the set of CIAO-config
-		 * properties associated with the route name.
-		 */
-		@SuppressWarnings("deprecation")
-		public void configure() {
-			final Registry registry = getContext().getRegistry();
-			final Object enricher = registry.lookupByName(enricherId);
-			final DocumentEnricherProcessor processor = DocumentEnricherProcessor.createProcessor(enricher);
-			
-			from("jms:queue:" + inputQueue)
-			.id("parse-document-" + name)
-			.streamCaching()
-			.doTry()
-				.unmarshal().json(JsonLibrary.Jackson, ParsedDocument.class)
-				.log(LoggingLevel.INFO, LOGGER, "Unmarshalled incoming JSON document")
-				.process(processor)					
-				.marshal().json(JsonLibrary.Jackson)
-				.to("jms:queue:" + outputQueue)
-			.doCatch(Exception.class)
-				.log(LoggingLevel.ERROR, LOGGER, "Exception while enriching document: ${file:name}")
-				.to("log:" + LOGGER.getName() + "?level=ERROR&showCaughtException=true")
-				.handled(false);
-		}
+		context.addRoutes(route);
 	}
 }
---
+++ ciao-docs-enricher/src/main/java/uk/nhs/ciao/docs/enricher/route/EnrichDocumentRoute.java
@@ -0,0 +1,97 @@
+package uk.nhs.ciao.docs.enricher.route;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.spi.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.nhs.ciao.camel.BaseRouteBuilder;
+import uk.nhs.ciao.configuration.CIAOConfig;
+import uk.nhs.ciao.docs.enricher.DocumentEnricherProcessor;
+import uk.nhs.ciao.docs.parser.ParsedDocument;
+import uk.nhs.ciao.exceptions.CIAOConfigurationException;
+
+/**
+ * Creates a Camel route for the specified name / property prefix.
+ * <p>
+ * Each configurable property is determined by:
+ * <ul>
+ * <li>Try the specific property: <code>${ROOT_PROPERTY}.${name}.${propertyName}</code></li>
+ * <li>If missing fallback to: <code>${ROOT_PROPERTY}.${propertyName}</code></li>
+ * </ul>
+ */
+public class EnrichDocumentRoute extends BaseRouteBuilder {
+	private static final Logger LOGGER = LoggerFactory.getLogger(EnrichDocumentRoute.class);
+	
+	/**
+	 * The root property 
+	 */
+	public static final String ROOT_PROPERTY = "documentEnricherRoutes";
+	
+	private final String name;
+	private final String inputQueue;
+	private final String enricherId;
+	private final String outputQueue;
+	private String inProgressFolderManagerUri;
+	
+	/**
+	 * Creates a new route builder for the specified name / property prefix
+	 * 
+	 * @param name The route name / property prefix
+	 * @throws CIAOConfigurationException If required properties were missing
+	 */
+	public EnrichDocumentRoute(final String name, final CIAOConfig config) throws CIAOConfigurationException {
+		this.name = name;
+		this.inputQueue = findProperty(config, "inputQueue");
+		this.enricherId = findProperty(config, "enricherId");
+		this.outputQueue = findProperty(config, "outputQueue");
+	}
+	
+	public void setInProgressFolderManagerUri(final String inProgressFolderManagerUri) {
+		this.inProgressFolderManagerUri = inProgressFolderManagerUri;
+	}
+	
+	/**
+	 * Try the specific 'named' property then fall back to the general 'all-routes' property
+	 */
+	private String findProperty(final CIAOConfig config, final String propertyName) throws CIAOConfigurationException {
+		final String specificName = ROOT_PROPERTY + "." + name + "." + propertyName;
+		final String genericName = ROOT_PROPERTY + "." + propertyName;
+		if (config.getConfigKeys().contains(specificName)) {
+			return config.getConfigValue(specificName);
+		} else if (config.getConfigKeys().contains(genericName)) {
+			
+			return config.getConfigValue(genericName);
+		} else {
+			throw new CIAOConfigurationException("Could not find property " + propertyName +
+					" for route " + name);
+		}
+	}
+
+	/**
+	 * Configures / creates a new Camel route corresponding to the set of CIAO-config
+	 * properties associated with the route name.
+	 */
+	@SuppressWarnings("deprecation")
+	@Override
+	public void configure() throws Exception {
+		final Registry registry = getContext().getRegistry();
+		final Object enricher = registry.lookupByName(enricherId);
+		final DocumentEnricherProcessor processor = DocumentEnricherProcessor.createProcessor(enricher);
+		
+		from("jms:queue:" + inputQueue)
+		.id("parse-document-" + name)
+		.streamCaching()
+		.doTry()
+			.unmarshal().json(JsonLibrary.Jackson, ParsedDocument.class)
+			.log(LoggingLevel.INFO, LOGGER, "Unmarshalled incoming JSON document")
+			.process(processor)					
+			.marshal().json(JsonLibrary.Jackson)
+			.to("jms:queue:" + outputQueue)
+		.doCatch(Exception.class)
+			.log(LoggingLevel.ERROR, LOGGER, "Exception while enriching document: ${file:name}")
+			.to("log:" + LOGGER.getName() + "?level=ERROR&showCaughtException=true")
+			.handled(false);
+	}
+}
